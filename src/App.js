7538237686e093b4f649bada47355a96b472d537
PraisesPJMT <praisesmusa@outlook.com>
2022-09-06 16:25:45 +0100

    Add Picture component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import ProfilePicture from './conponents/Picture';
+
+const App = () => (
+  <>
+    <ProfilePicture />
+  </>
+);
+
+export default App;
