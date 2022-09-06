b005113ffbee30196d9a336abf016cc805bc3558
PraisesPJMT <praisesmusa@outlook.com>
2022-09-06 17:00:28 +0100

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import ProfilePicture from './conponents/Picture';
+import Header from './conponents/Header';
 
 const App = () => (
   <>
     <ProfilePicture />
+    <Header />
   </>
 );
 
---
+++ src/conponents/Header.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const Header = () => (
+  <>
+    <h1 className="title">
+      Praises Musa Tula
+    </h1>
+    <h2 className="sub-title">
+      Full-Stack Developer
+    </h2>
+    <a
+      className="portfolio"
+      href="https://praisespjmt.github.io/Praises-Tula-Portfolio/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Praises-Tula-Portfolio
+    </a>
+    <div className="quick-link">
+      <a
+        className="btn email-btn"
+        href="mailto:praisesmusa@gmail.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon
+          className="email"
+          icon={faEnvelope}
+        />
+        <span>Email</span>
+      </a>
+      <a
+        className="btn linkedin-btn"
+        href="https://www.linkedin.com/in/praises-tula-9233aa76"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon
+          className="linkedin"
+          icon={faLinkedin}
+        />
+        <span>LinkedIn</span>
+      </a>
+    </div>
+  </>
+);
+
+export default Header;
